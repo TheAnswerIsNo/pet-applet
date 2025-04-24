9cae1ee42f2080a6ebcc2988f7a9a44d9f0d397a
yangyang <2030819784@qq.com>
2025-04-24 22:50:27 +0800

    feat: 订单相关，我的相关

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,8 +1,11 @@
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
 import Taro from '@tarojs/taro'
+import { Avatar, Cell } from '@nutui/nutui-react-taro'
 
 export default function Index() {
+
+  const user = Taro.getStorageSync('user')
   useLoad(() => {
     console.log('Page loaded.')
   })
@@ -14,10 +17,25 @@ export default function Index() {
     })
   }
 
+
+  const orderListManage = () => {
+    Taro.navigateTo({
+      url: '/pages/orderList/index',
+    })
+  }
+
   return (
     <div className="index">
-      <div onClick={addressManage}>
-        地址管理
+      <div style={{ padding: '10px', display: 'flex', alignItems: 'center' }}>
+        <Avatar color="rgb(245, 106, 0)" style={{ background: 'rgb(253, 227, 207)' }}>U</Avatar>
+        <text style={{ marginLeft: '12px' }}>{user.nickname || '游客'}</text>
+      </div>
+      <div>
+        <Cell title="地址管理" onClick={addressManage} />
+        <Cell title="订单管理" onClick={orderListManage} />
+        <Cell title="我的领养" onClick={addressManage} />
+        <Cell title="我的送养" onClick={addressManage} />
+        <Cell title="我的动态" onClick={addressManage} />
       </div>
 
     </div>
